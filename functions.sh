f924f7b304eefbb9aedc013ce96138a35ceab8a6
RicardoTejera <ricardo.mdltp@gmail.com>
2025-10-09 00:32:03 -0600

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -10,11 +10,11 @@ pow() {
         result=$((result * a))
     done
 
-    echo $result
+    echo "$result"
 }
 
+# Function to find the shortest string
 shortest() {
-
     # Print each string on a new line if more than 2 arguments
     if [[ $# -gt 2 ]]; then
         for arg in "$@"; do
@@ -32,30 +32,3 @@ shortest() {
 
     echo "$min"
 }
-
-# Check that at least one argument (function name) is given
-if [[ $# -lt 1 ]]; then
-    echo "Usage:"
-    echo "  $0 pow <base> <exponent>"
-    echo "  $0 shortest <string1> <string2> [string3 ...]"
-    exit 1
-fi
-
-# Extract function name (first argument)
-func="$1"
-shift  # remove the function name from the arguments list
-
-# Call the requested function with remaining arguments
-case "$func" in
-    pow)
-        pow "$@"
-        ;;
-    shortest)
-        shortest "$@"
-        ;;
-    *)
-        echo "Unknown function: $func"
-        echo "Available: pow, shortest"
-        exit 1
-        ;;
-esac
