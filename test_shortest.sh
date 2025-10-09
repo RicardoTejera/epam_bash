9f00afc2c0d85c12de8296359f2125654ea4d7c3
RicardoTejera <ricardo.mdltp@gmail.com>
2025-10-09 00:31:46 -0600

    Update test_shortest.sh

--- test_shortest.sh
+++ test_shortest.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-shortest() {
+# Import the functions
+source ./functions.sh
 
-    # Print each string on a new line if more than 2 arguments
-    if [[ $# -gt 2 ]]; then
-        for arg in "$@"; do
-            echo "$arg"
-        done
-    fi
+# Allow calling functions manually
+func="$1"
+shift
 
-    # Find the shortest string
-    local min="${1}"
-    for arg in "$@"; do
-        if [[ ${#arg} -lt ${#min} ]]; then
-            min="$arg"
-        fi
-    done
+case "$func" in
 
-    echo "$min"
-}
-
-shortest "$@"
+    shortest)
+        shortest "$@"
+        ;;
+    *)
+        echo "Usage:"
+        echo "  $0 shortest <string1> <string2> [string3 ...]"
+        ;;
+esac
