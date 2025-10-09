a8cb6467e9cc497b036fcdcc010a621fd648d279
RicardoTejera <ricardo.mdltp@gmail.com>
2025-10-08 23:37:08 -0600

    w

--- functions.sh
+++ functions.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-# Function to calculate integer powers
-pow() {
-    local a=$1
-    local b=$2
-    local result=1
 
-    for ((i=0; i<b; i++)); do
-        result=$((result * a))
+
+########################################################################
+
+#!/bin/bash
+
+shortest() {
+
+    # Print each string on a new line if more than 2 arguments
+    if [[ $# -gt 2 ]]; then
+        for arg in "$@"; do
+            echo "$arg"
+        done
+    fi
+
+    # Find the shortest string
+    local min="${1}"
+    for arg in "$@"; do
+        if [[ ${#arg} -lt ${#min} ]]; then
+            min="$arg"
+        fi
     done
 
-    echo $result
+    echo "$min"
 }
 
-# Check that exactly 2 arguments are passed
-if [[ $# -ne 2 ]]; then
-    echo "Usage: $0 <base> <exponent>"
-    exit 1
-fi
 
-# Call the function with command-line arguments
-pow "$1" "$2"
+
